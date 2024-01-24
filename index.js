6d601e182eca328556235df6cfb5286a225b6d4a
Reilly Shofner <reilly.shofner@gmail.com>
2024-01-23 20:49:50 -0800

    complete dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove('main');
+
+const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+
+newHeader.id=('victory');
+newHeader.innerHTML = 'Reilly is the champion';
+document.body.append(newHeader);
+
